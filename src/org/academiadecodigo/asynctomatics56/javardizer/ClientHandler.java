9ccaef7dd20c6f3da83490d36a52348948661c50
ConGito <joaofariaemello@gmail.com>
2021-02-27 03:13:30 +0000

    added user mapping + /quit command lul :)

--- src/org/academiadecodigo/asynctomatics56/javardizer/ClientHandler.java
+++ src/org/academiadecodigo/asynctomatics56/javardizer/ClientHandler.java
@@ -1,20 +1,23 @@
 package org.academiadecodigo.asynctomatics56.javardizer;
 
 
+import org.academiadecodigo.bootcamp.Prompt;
+import org.academiadecodigo.bootcamp.scanners.string.StringInputScanner;
+
 import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
-import java.util.List;
+import java.util.Map;
 
 
 public class ClientHandler implements Runnable {
 
     private Socket clientSocket;
     private BufferedReader inputReader;
-    private List<Socket> connectedUsers;
+    private Map<Socket, String> connectedUsers;
 
 
-    public ClientHandler(Socket clientSocket, List<Socket> connectedUsers) {
+    public ClientHandler(Socket clientSocket, Map<Socket, String> connectedUsers) {
         this.clientSocket = clientSocket;
         this.connectedUsers = connectedUsers;
     }
@@ -35,22 +38,27 @@ public class ClientHandler implements Runnable {
 
     private void serve() throws IOException {
 
+        addClient();
+
         while (!clientSocket.isClosed()) {
             readAndSend();
         }
     }
 
-    private void readAndSend() throws IOException {
+    private synchronized void readAndSend() throws IOException {
 
         inputReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String message = inputReader.readLine() + "\n";
-        System.out.println("Sending Message to User");
-        sendMessage(message);
+        if (inputReader.readLine().equals("/quit")) {
+            System.exit(0);
+        } else {
+            String message = inputReader.readLine() + "\n";
+            sendMessage(message);
+        }
     }
 
     private void sendMessage(String message) throws IOException {
 
-        for (Socket s : connectedUsers) {
+        for (Socket s : connectedUsers.keySet()) {
             if (s != null && !s.equals(this.clientSocket)) {
                 OutputStream os = s.getOutputStream();
                 os.write(message.getBytes());
@@ -59,13 +67,40 @@ public class ClientHandler implements Runnable {
         }
     }
 
+    private String createUsername() throws IOException {
+
+        InputStream in = clientSocket.getInputStream();
+        PrintStream out = new PrintStream(clientSocket.getOutputStream());
+        Prompt prompt = new Prompt(in, out);
+        StringInputScanner scn = new StringInputScanner();
+        scn.setMessage("Enter a username: ");
+        return prompt.getUserInput(scn);
+
+
+    }
+
+    private void addClient() throws IOException {
+
+        String username = createUsername();
+
+        if (!connectedUsers.containsValue(username)) {
+            connectedUsers.put(clientSocket, username);
+            System.out.println(connectedUsers.containsValue("Giefweed"));
+        } else {
+            String s = "ERROR: " + username + " already in use!";
+            OutputStream os = clientSocket.getOutputStream();
+            os.write(s.getBytes());
+            os.flush();
+            os.close();
+        }
+    }
 
     private void cleanup(Closeable closeable) {
 
         if (closeable != null) {
             try {
                 closeable.close();
-                for (Socket s : connectedUsers) {
+                for (Socket s : connectedUsers.keySet()) {
                     if (s.equals(this.clientSocket) && s.isClosed()) {
                         connectedUsers.remove(s);
                     }
--- src/org/academiadecodigo/asynctomatics56/javardizer/Server.java
+++ src/org/academiadecodigo/asynctomatics56/javardizer/Server.java
@@ -1,12 +1,15 @@
 package org.academiadecodigo.asynctomatics56.javardizer;
 
 
+
+
 import java.io.Closeable;
 import java.io.IOException;
+
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -15,18 +18,19 @@ public class Server {
 
     private ServerSocket serverSocket;
     private Socket clientSocket;
-    private List<Socket> connectedUsers;
+    private Map<Socket, String> connectedUsers;
     private final int NUM_THREADS = 200;
+    ClientHandler clientHandler;
 
     public Server(int port) {
 
-        connectedUsers = new ArrayList<>();
+        connectedUsers = new HashMap<>();
+
         try {
             openConnection(port);
             while (true) {
                 listen(serverSocket);
                 createNewHandler(clientSocket);
-                addClient();
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -40,12 +44,14 @@ public class Server {
         serverSocket = new ServerSocket(port);
     }
 
-    private void createNewHandler(Socket clientSocket) {
+    private void createNewHandler(Socket clientSocket) throws IOException {
 
         System.out.println("Creating new Handler for " + clientSocket);
         ExecutorService executor = Executors.newFixedThreadPool(NUM_THREADS);
-        ClientHandler clientHandler = new ClientHandler(clientSocket, connectedUsers);
+        clientHandler = new ClientHandler(clientSocket, connectedUsers);
         executor.submit(clientHandler);
+
+
     }
 
     private void listen(ServerSocket serverSocket) throws IOException {
@@ -55,11 +61,7 @@ public class Server {
         System.out.println("Client accepted: " + clientSocket);
     }
 
-    private void addClient() throws IOException {
 
-        System.out.println("Adding new Client " + clientSocket);
-        connectedUsers.add(clientSocket);
-    }
 
     private void cleanup(Closeable closeable) {
 
