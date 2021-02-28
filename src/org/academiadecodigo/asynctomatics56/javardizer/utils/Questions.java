d09bb79f92dba7ce2e6fe958167590bb65b63133
ConGito <joaofariaemello@gmail.com>
2021-02-28 19:53:29 +0000

    small question bugfixes

--- src/org/academiadecodigo/asynctomatics56/javardizer/utils/Questions.java
+++ src/org/academiadecodigo/asynctomatics56/javardizer/utils/Questions.java
@@ -3,55 +3,55 @@ package org.academiadecodigo.asynctomatics56.javardizer.utils;
 
 public class Questions {
 
-    public static final String Q1 = "Which is the correct order of the software development lifecycle?";
+    public static final String Q1 = "What is the software development cycle's correct order?";
     public static final String Q1_ANS_A = "Requirement analysis -> Design -> Implementation -> Test -> Support"; // Correct answer
     public static final String Q1_ANS_B = "Support -> Design -> Requirement analysis -> Implementation -> Test";
     public static final String Q1_ANS_C = "Design -> Requirement analysis -> Test -> Implementation -> Support";
     public static final String Q1_ANS_D = "Requirement analysis -> Design -> Implementation -> Support ->  Test";
 
-    public static final String Q2 = "Which are the three types of tests?";
+    public static final String Q2 = "What are the three types of tests?";
     public static final String Q2_ANS_A = "Blood tests, integration tests and acceptance tests";
     public static final String Q2_ANS_B = "Unity tests, integration tests and talent tests";
     public static final String Q2_ANS_C = "Unity tests, personality tests and acceptance tests";
     public static final String Q2_ANS_D = "Unity tests, integration tests and acceptance tests"; // Correct answer
 
-    public static final String Q3 = " Which option describes the acceptance tests?";
-    public static final String Q3_ANS_A = "Test if the software work on Windows Millennium Edition";
-    public static final String Q3_ANS_B = "Tests if the code units work well together to accomplish software features.";
-    public static final String Q3_ANS_C = "Test if the software meets the requirements and the client expectations."; // Correct answer
+    public static final String Q3 = " What option describes the acceptance tests?";
+    public static final String Q3_ANS_A = "Test if the software works on Windows Millennium Edition";
+    public static final String Q3_ANS_B = "Test if the code units work well together to accomplish software features.";
+    public static final String Q3_ANS_C = "Test if the software meets the requirements and the client's expectations."; // Correct answer
     public static final String Q3_ANS_D = "Tests if individual units of code (Classes, Methods) are working correctly.";
 
-    public static final String Q4 = "Which are the most known and used development methodologies?";
+    public static final String Q4 = "What are the most well known and used development methodologies?";
     public static final String Q4_ANS_A = "River and Agile";
     public static final String Q4_ANS_B = "Waterfall and Agile"; // Correct answer
     public static final String Q4_ANS_C = "Lake and Lazy";
     public static final String Q4_ANS_D = "Waterfall and Abarth";
 
-    public static final String Q5 = "Regarding the Waterfall model, please select the correct answer.";
+    public static final String Q5 = "Regarding the Waterfall model, please select the correct answer:";
     public static final String Q5_ANS_A = "Changes to previous phases are easily done and recommended";
     public static final String Q5_ANS_B = "One lifecycle phase does not start while the previous is not completely done."; // Correct
     public static final String Q5_ANS_C = "The next phase can start even if the previous is not completely done.";
     public static final String Q5_ANS_D = "The project documentation does not need to be thoroughly documented";
 
-    public static final String Q6 = "Regarding the Agile model, please select the correct answer.";
+    public static final String Q6 = "Regarding the Agile model, please select the correct answer:";
     public static final String Q6_ANS_A = "Changes are welcome and seen as a natural process of a product evolution"; // Correct answer
     public static final String Q6_ANS_B = "It is extremely hard to implement new features and correct issues";
     public static final String Q6_ANS_C = "Single iteration of the software lifecycle";
     public static final String Q6_ANS_D = "The next phase can not start before the previous one has ended.";
 
-    public static final String Q7 = "Each scrum team has 3 different key roles, which are:";
+    public static final String Q7 = "Each scrum team has 3 different key roles. These are:";
     public static final String Q7_ANS_A = "Product Owner, Development Team, Cleaning Lady";
     public static final String Q7_ANS_B = "Stakeholders, Product Owner, Development Team";
     public static final String Q7_ANS_C = "Product Owner, Human Resources Team, Development Team";
     public static final String Q7_ANS_D = "Product Owner, Scrum Master, Development Team"; // Correct answer
 
-    public static final String Q8 = "Regarding Kanban, please select the correct answer";
+    public static final String Q8 = "Regarding Kanban, please select the correct answer:";
     public static final String Q8_ANS_A = "Kanban is based on sprints";
     public static final String Q8_ANS_B = "Kanban does no put any limits on how many features a team can work on at a given time.";
     public static final String Q8_ANS_C = "Kanban means: sign and board, that is, a board with signs (tasks) and its state (To Do, Doing, Done) "; // Correct answer
-    public static final String Q8_ANS_D = "Kanban is a Japanese dish.";
+    public static final String Q8_ANS_D = "Kanban is a Japanese hooker.";
 
-    public static final String Q9 = "Regarding Lean, please select the correct answer";
+    public static final String Q9 = "Regarding Lean, please select the correct answer:";
     public static final String Q9_ANS_A = "Lean is the best development methodology that a start-up can use"; // Correct answer
     public static final String Q9_ANS_B = "It is extremely hard to implement new features and correct issues";
     public static final String Q9_ANS_C = "Single iteration of the software lifecycle";
